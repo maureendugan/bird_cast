cd81cca0b5705752af22eff26434c000d3a1b748
Maureen Dugan & Liz Tom <student@epicodus.com>
2014-04-15 09:15:39 -0700

    initial commit

---
+++ Gemfile
@@ -0,0 +1,35 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.4'
+gem 'pg'
+gem 'sass-rails', '~> 4.0.2'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.2'
+gem 'bootstrap-sass'
+gem 'paperclip', github: 'thoughtbot/paperclip'
+gem 'rghost'
+gem 'bcrypt'
+
+group :development do
+  gem 'better_errors'
+  gem 'binding_of_caller'
+  gem 'quiet_assets'
+end
+
+group :test, :development do
+  gem 'rspec-rails'
+  gem 'pry'
+  gem 'factory_girl_rails'
+  gem 'capybara'
+  gem 'database_cleaner'
+  gem 'launchy'
+  gem 'letter_opener'
+
+end
+
+group :test do
+  gem 'shoulda-matchers'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BirdCast::Application.config.secret_key_base = '308b23a39ea0cead09172f598c2f54c66180d439f5897c4570e2decb6a95d0f318b36636c7bb363d388f6431e1c0c1d28d099cc375892fed2f04867572b428d0'
