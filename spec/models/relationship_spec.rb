4fde9660653617bb67536ee22a29511352eaef9b
Angela Yen & Maureen Dugan <student@epicodus.com>
2014-04-18 14:32:29 -0700

    add follower/followed associations and validations to relationships

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -12,8 +12,25 @@ describe Relationship do
   it 'creates a relationship between two users' do
     user_follower = FactoryGirl.create(:user)
     user_followed = FactoryGirl.create(:user)
-    # Relationship.create({follower_id: user_follower.id, followed_id: user_followed.id})
     user_follower.relationships.create(followed_id: user_followed.id)
     Relationship.find_by_follower_id(user_follower.id).followed_id.should eq user_followed.id
   end
+
+  describe "follower methods" do
+    it { should respond_to(:follower) }
+    it { should respond_to(:followed) }
+    its(:follower) { should eq follower }
+    its(:followed) { should eq followed }
+  end
+
+  describe "when followed id is not present" do
+    before { relationship.followed_id = nil }
+    it { should_not be_valid }
+  end
+
+  describe "when follower id is not present" do
+    before { relationship.follower_id = nil }
+    it { should_not be_valid }
+  end
+
 end
