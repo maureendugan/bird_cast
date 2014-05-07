1301aaba69173a4fddeabc2cb2f04e4f1bdf32a6
Micah Geisel <micah@botandrose.com>
2014-05-07 16:21:04 -0700

    refactor create new user account test

--- spec/features/user_page_spec.rb
+++ spec/features/user_page_spec.rb
@@ -1,14 +1,14 @@
 require 'spec_helper'
 
-describe User do
-  it 'allows a new user to create an account' do
-    visit new_user_path
-    user = FactoryGirl.build(:user)
-    fill_in :user_name, with: user.name
-    fill_in :user_username, with: user.username
+feature 'User authentication' do
+  scenario 'new user creates an account' do
+    visit '/'
+    click_link 'Sign Up'
+    fill_in :user_name, with: 'Ernie'
+    fill_in :user_username, with: 'Ernie'
     fill_in :user_password, with: 'password'
     fill_in :user_password_confirmation, with: 'password'
-    fill_in :user_email, with: user.email
+    fill_in :user_email, with: 'ernie@email.com'
     click_button "Create User"
     page.should have_content "Your account has been created"
   end
