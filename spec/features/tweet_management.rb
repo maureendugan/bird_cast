c1cf99f6d576155afb8e1332c4aebaf46b0750ae
Maureen <maureen.k.dugan@gmail.com>
2014-05-08 11:01:18 -0700

    redo tweet management tests

---
+++ spec/features/tweet_management.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+feature 'User manages tweets' do
+  scenario 'lets an authorized user post a new tweet' do
+    sign_in(FactoryGirl.create(:user, username: 'Ernest'))
+
+    fill_in 'tweet-box', with: 'This is a great tweet'
+    click_button 'Create Tweet'
+    page.should have_content 'Ernest This is a great tweet'
+  end
+  scenario 'user deletes their own tweet' do
+    sign_in(FactoryGirl.create(:user, username: 'Evelyn'))
+
+    fill_in 'tweet-box', with: 'this tweet should be deleted'
+    click_button 'Create Tweet'
+    click_link 'Delete'
+    page.should_not have_content 'Evelyn this tweet should be deleted'
+  end
+end
+
+describe Tweet do
+
+  it "deletes a tweet" do
+    tweet = FactoryGirl.build(:tweet)
+    create_sign_in_user_create_tweet(tweet)
+    click_link 'Delete'
+    page.should have_no_content "#{tweet.text}"
+  end
+
+end
